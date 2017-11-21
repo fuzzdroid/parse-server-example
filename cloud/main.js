97d096d0865a764fbfa5e426fb89f9b66643901e
fuzzdroid <hello@playfuzzword.com>
2017-11-21 17:45:33 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -4,16 +4,35 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 
-Parse.Cloud.define('heartUser', function(req, res) {
+Parse.Cloud.define(“heartUser”, function(request, response) {
+  var query = new Parse.Query(Parse.User); 
 
-Parse.Cloud.useMasterKey();
-               var user = new Parse.Object('User');
-               user.userName = req.params.userName;
-               user.increment('hearts');
-               user.save;
-  res.success('yo');
+      query.equalTo("objectId", request.params.userName);
+      
+      query.first({
+            success: function(object) {
 
-});
+               object.increment("hearts");
 
+                  //do something to the object
+
+                  object.save(null, { useMasterKey: true }).then(
+                        function (object) {
+                              console.log("Object Saved Successfully");  
+                        },
+                        function (error) {
+                              console.log("Error Saving Object -- Response Error");        
+                        }
+                  );
+
+                  console.log(JSON.stringify(object));                   response.success(object);
+            },
+            error: function(error) {
+                  response.error("Error Message");
+            }
+      });
+
+
+  });
 
 
