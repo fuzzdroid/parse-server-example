25ed6da62a3994579560cd3041b31a78599abcdc
fuzzdroid <hello@playfuzzword.com>
2017-11-21 17:22:27 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,13 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
+Parse.Cloud.define('heartUser', function(req, res) {
+
+Parse.Cloud.useMasterKey();
+               var user = new Parse.Object("User");
+               user.userName = req.params.userName;
+               user.increment("hearts");
+               user.save;
+  res.success('yo');
+
+});
