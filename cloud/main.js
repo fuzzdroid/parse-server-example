3f4b350738c30198ed968734b3bd8c56b9350594
fuzzdroid <hello@playfuzzword.com>
2017-11-21 16:21:42 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,14 +8,13 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define('heartIt', function(request, response) {
    query = new Parse.Query(“User”);
-                  query.get(request.object.get(“itemId"), {
+                  query.get(request.object.get('itemId'), {
                             success: function(post) {
                             post.increment(“hearts”, 1);
                             post.save();
                             response.success();
                             },
                             error: function(error) {
-                            console.error("Got an error " + error.code + " : " + error.message);
                             response.error();
                             }
                             });
