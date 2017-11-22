f2fe24ef5c138fd1aa96c3491ba81af18d7a056b
fuzzdroid <hello@playfuzzword.com>
2017-11-22 14:37:36 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -5,18 +5,18 @@ Parse.Cloud.define('hello', function(req, res) {
 
 
 Parse.Cloud.define('heartTap', function(request, response) {
-  var SaveObject = Parse.Object.extend(“User”);
+  var SaveObject = Parse.Object.extend('User');
   var saveObject = new Parse.Query(SaveObject);
-  saveObject.equalTo("objectId", request.params.userId);
+  saveObject.equalTo('objectId', request.params.userId);
   saveObject.first({
     useMasterKey: true,
     success: function(Objects) {
       Objects.save(null, {
         useMasterKey: true,
         success: function(object) {
-          object.increment(“hearts”);
+          object.increment('hearts');
           object.save();
-          console.log("Cloud Code: hearts rating has increased by 1.", object);
+          console.log('Cloud Code: hearts rating has increased by 1.', object);
           response.success('Cloud Code: hearts rating has increased by 1.');
 
         }
