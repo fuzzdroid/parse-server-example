c69b559f571cc47ddbe78dd22573f6abbb7fb52a
fuzzdroid <hello@playfuzzword.com>
2017-11-22 15:17:37 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,25 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('heartTap', function(request, response) {
+  var SaveObject = Parse.Object.extend(Parse.User);
+  var saveObject = new Parse.Query(SaveObject);
+  saveObject.equalTo('objectId', request.params.userId);
+  saveObject.first({
+    useMasterKey: true,
+    success: function(Objects) {
+      Objects.save(null, {
+        useMasterKey: true,
+        success: function(object) {
+          object.increment('hearts');
+          object.save();
+          console.log('Cloud Code: hearts rating has increased by 1.', object);
+          response.success('Cloud Code: hearts rating has increased by 1.');
+
+        }
+      });
+    }
+  });
+
+});
